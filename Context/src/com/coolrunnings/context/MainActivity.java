2867b9b77328add23e27b788dd4e3b0bc6e733b8
Samuel Gonshaw <sjg10@imperial.ac.uk>
2014-03-08 15:22:20 +0000

    Install tessdata begun

--- Context/src/com/coolrunnings/context/MainActivity.java
+++ Context/src/com/coolrunnings/context/MainActivity.java
@@ -18,11 +18,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		/*
-		 * We're creating a card for the interface.
-		 * 
-		 * More info here: http://developer.android.com/guide/topics/ui/themes.html
-		 */
+		//TODO: Add tessdata file install
 		Card card1 = new Card(this);
 		card1.setText("Hello, Sir!"); // Main text area
 		card1.setFootnote("..or Ma'am"); // Footer

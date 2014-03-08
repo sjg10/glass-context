f401097f58d61f48c50c816640b2f0bae120a1da
Samuel Gonshaw <sjg10@imperial.ac.uk>
2014-03-08 14:28:00 +0000

    Initial imageproc

--- Context/src/com/coolrunnings/context/ImageProc.java
+++ Context/src/com/coolrunnings/context/ImageProc.java
@@ -1,5 +1,28 @@
 package com.coolrunnings.context;
 
-public class ImageProc {
+	public class ImageProc{
+/*
+		public static String convertImageToString(byte[] data){
+				data=tidyImage(data);
+				return textFromTidyImage(data);
+		}
 
+		public static byte[] tidyImage(byte[] data){
+			//TODO: Use an imageproc package to run through.
+			return data;
+		}
+
+		private static String textFromTidyImage(byte[] data){
+				TessBaseAPI baseApi = new TessBaseAPI();
+				baseApi.init(cameraAct.getFilesDir().getAbsolutePath(), "eng");
+				//baseApi.setVariable("tessedit_char_whitelist", "123456789");
+				baseApi.setPageSegMode(PageSegMode.PSM_SINGLE_CHAR);
+				int[][] grid=new int[9][9];
+				int inc=size/9;
+				Bitmap bmp = Bitmap.createBitmap(inc-16, inc-16,Bitmap.Config.ARGB_8888);
+				baseApi.setImage(bmp);
+				return baseApi.getUTF8Text();
+				baseApi.clear();
+		}
+	}*/
 }

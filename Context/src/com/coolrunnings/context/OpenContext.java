76c0b74e011b61b8417e8d84f4d2e858758fab8f
Yi Zhang <yi.zhang7210@gmail.com>
2014-03-08 13:46:01 +0000

    Initial Commit with empty classes.

--- Context/src/com/coolrunnings/context/OpenContext.java
+++ Context/src/com/coolrunnings/context/OpenContext.java
@@ -16,5 +16,20 @@ import android.content.Intent;
 
 
 public class OpenContext extends Service{
-
+	
+	// Name of our live card:
+	private static final String LIVE_CARD_ID = "Context_Live_Card";
+	
+	// The time line manager for adding cards to the time line:
+	private TimelineManager mTimelineManager;
+	
+	// Create the live card:
+	@SuppressWarnings("unused")
+	private LiveCard mLiveCard;
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		mTimelineManager = TimelineManager.from(this);
+	} // onCreate
 }
